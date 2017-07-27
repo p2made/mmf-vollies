8b718a5f1c9a7c95d499af25921d5ce09f065a33
Pedro Plowman <p2opensource@gmail.com>
2017-07-28 03:53:08 +1000

    teams & jobs models

--- backend/models/JobSearch.php
+++ backend/models/JobSearch.php
@@ -9,36 +9,45 @@ use backend\models\Job;
 
 /**
  * JobSearch represents the model behind the search form about `backend\models\Job`.
+ *
+ * @property integer $id
+ * @property integer $team_id
+ * @property integer $group_id
+ * @property integer $sequence
+ * @property string $name
+ * @property string $shortName
+ * @property string $description
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property \common\models\Application[] $applications
+ * @property \common\models\Application[] $applications0
+ * @property \common\models\Application[] $applications1
+ * @property \common\models\MenuGroup $group
+ * @property \common\models\Team $team
+ * @property string $aliasModel
+ *
+ * @property string $teamName
  */
 class JobSearch extends Job
 {
-	/**
-	 * @inheritdoc
-	 */
+	// virtual attributes
+	public $teamName;
+
 	public function rules()
 	{
 		return [
 			[['id', 'team_id', 'group_id', 'sequence', 'created_at', 'updated_at'], 'integer'],
-			[['name', 'shortName', 'description'], 'safe'],
+			[['teamName', 'name', 'shortName', 'description'], 'safe'],
 		];
 	}
 
-	/**
-	 * @inheritdoc
-	 */
 	public function scenarios()
 	{
 		// bypass scenarios() implementation in the parent class
 		return Model::scenarios();
 	}
 
-	/**
-	 * Creates data provider instance with search query applied
-	 *
-	 * @param array $params
-	 *
-	 * @return ActiveDataProvider
-	 */
 	public function search($params)
 	{
 		$query = Job::find();
@@ -68,8 +77,27 @@ class JobSearch extends Job
 		]);
 
 		$query->andFilterWhere(['like', 'name', $this->name])
-			->andFilterWhere(['like', 'shortName', $this->shortName])
 			->andFilterWhere(['like', 'description', $this->description]);
+
+		$dataProvider->setSort([
+			'attributes' => [
+				'team_id',
+				'sequence',
+				'name',
+				'shortName',
+				'teamName' => [
+					'asc' => ['mmf_team.name' => SORT_ASC],
+					'desc' => ['mmf_team.name' => SORT_DESC],
+					'label' => 'Team Name'
+				]
+			],
+        	'defaultOrder' => ['team_id' => SORT_ASC, 'sequence' => SORT_ASC]
+		]);
+
+		// filter by team name
+		$query->joinWith(['team' => function ($q) {
+			$q->where('mmf_team.name LIKE "%' . $this->teamName . '%"');
+		}]);
 
 		return $dataProvider;
 	}
